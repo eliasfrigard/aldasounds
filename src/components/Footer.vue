40aea9da957051b8216d06e80f112ee5e46a921b
Elias Frigård <ef222xf@student.lnu.se>
2021-11-22 15:21:22 +0200

    replaced downloads with listen to in footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,16 +15,23 @@
           </div>
         </div>
         <div id="downloads" class="five wide column">
-          <h4 class="ui inverted header">Downloads</h4>
+          <h4 class="ui inverted header">Listen To Our Music</h4>
           <div class="ui hidden divider"></div>
           <div class="ui inverted link list">
+
+            <a target="_blank" href="https://open.spotify.com/album/6ewLQQEhzrgIvEOXWtlYPV?si=rbuc5Ux3QTeIF5Cwxgh9_g&dl_branch=1&nd=1" class="item">Spotify</a>
+            <a target="_blank" href="https://aldasounds.bandcamp.com/releases" class="item">Bandcamp</a>
+            <a target="_blank" href="https://music.apple.com/fi/album/nine-worlds/1582222096" class="item">Apple iTunes</a>
+            <a target="_blank" href="https://www.amazon.com/Nine-Worlds-Alda/dp/B09D8SXWKT/ref=sr_1_1?dchild=1&keywords=alda+nine+worlds&qid=1630664054&rnid=2941120011&s=music&sr=1-1" class="item">Amazon Music</a>
+
+            <div class="ui hidden divider"></div>
 <!--             <a href="#contact-container" class="item">Download Presskit</a>
             <a href="https://github.com/eliasfrigard/devsite" target="_blank" class="item">Download Technical Rider</a> -->
-            <a href="/music/ALDA Music.zip" class="item" v-if="!mobile" download="ALDA Music">Download Full Audio Samples</a>
+<!--             <a href="/music/ALDA Music.zip" class="item" v-if="!mobile" download="ALDA Music">Download Full Audio Samples</a>
             <a href="/music/ALDA Music.zip" class="item" v-else download="ALDA Music">Download Audio Sample</a>
             <a href="/originals/ALDA Images.zip" class="item" v-if="!mobile" download="ALDA Images">Download Full Resolution Images</a>
             <a href="/originals/ALDA Images.zip" class="item" v-else download="ALDA Images">Download Images</a>         
-            <br>
+ -->            <br>
           </div>
         </div>
 
